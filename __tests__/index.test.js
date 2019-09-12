be9b5e8a1f4683da3760c38cef54b9a6b0a3e993
helloitsjoe <jboyle617@gmail.com>
2019-09-12 19:50:01 -0400

    Ignore __tests__ subdirectories for coverage

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,14 @@
-const { makeJestConfig, defaultTestMatch } = require('../index');
+const { makeJestConfig, defaultTestMatch } = require("../index");
 
-describe('Main config', () => {
-  it('Runs with defaults', () => {
+describe("Main config", () => {
+  it("Runs with defaults", () => {
     expect(makeJestConfig()).toMatchInlineSnapshot(`
       Object {
         "clearMocks": true,
         "collectCoverage": true,
         "collectCoverageFrom": Array [
           "./src/**/*.js",
-          "!**/__tests__",
+          "!**/__tests__/**",
           "!/node_modules",
         ],
         "coverageDirectory": "coverage",
@@ -34,33 +34,33 @@ describe('Main config', () => {
     `);
   });
 
-  it('collectCoverage = false does not include any coverage properties', () => {
+  it("collectCoverage = false does not include any coverage properties", () => {
     expect(makeJestConfig({ collectCoverage: false })).toMatchInlineSnapshot(`
-      Object {
-        "clearMocks": true,
-        "moduleNameMapper": Object {
-          "\\\\.(css|less|scss)$": "identity-obj-proxy",
-        },
-        "setupFiles": Array [],
-        "setupFilesAfterEnv": Array [],
-        "testMatch": Array [
-          "**/__tests__/*.(test|spec|jest).js",
-        ],
-        "testPathIgnorePatterns": Array [
-          "__tests__/(setup|testUtils).js",
-        ],
-        "verbose": true,
-      }
-    `);
+            Object {
+              "clearMocks": true,
+              "moduleNameMapper": Object {
+                "\\\\.(css|less|scss)$": "identity-obj-proxy",
+              },
+              "setupFiles": Array [],
+              "setupFilesAfterEnv": Array [],
+              "testMatch": Array [
+                "**/__tests__/*.(test|spec|jest).js",
+              ],
+              "testPathIgnorePatterns": Array [
+                "__tests__/(setup|testUtils).js",
+              ],
+              "verbose": true,
+            }
+        `);
   });
 
-  it('can replace array fields', () => {
-    const testMatch = ['some regex'];
+  it("can replace array fields", () => {
+    const testMatch = ["some regex"];
     expect(makeJestConfig({ testMatch }).testMatch).toEqual(testMatch);
   });
 
-  it('can merge array fields', () => {
-    const myTestMatch = ['some regex'];
+  it("can merge array fields", () => {
+    const myTestMatch = ["some regex"];
     const testMatch = [...defaultTestMatch, ...myTestMatch];
     expect(makeJestConfig({ testMatch }).testMatch).toEqual(testMatch);
   });
