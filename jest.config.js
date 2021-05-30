d704800553085d5b25cdf602409973c153b6cf83
helloitsjoe <jboyle617@gmail.com>
2021-05-30 07:13:50 -0400

    back to default jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
 const { makeJestConfig } = require('./index');
 
-// Undefined default testEnvironment uses jest defaults
-module.exports = makeJestConfig({ testEnvironment: 'jsdom' });
+module.exports = makeJestConfig();
